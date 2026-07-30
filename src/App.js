a3a70b309586e3b746b98180f33a9c097ef031a8
Priyavrat Misra <priyavratmisra123@gmail.com>
2026-07-30 18:21:28 +0530

    create components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import React from "react";
+
 function App() {
-  return <p>Hello, World!</p>;
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Pizza Stop</h1>
+    </header>
+  );
+}
+
+function Body() {
+  return (
+    <main>
+      <h2>Menu</h2>
+      <h2>Address</h2>
+      <h2>Cart</h2>
+    </main>
+  );
+}
+
+function Footer() {
+  return React.createElement(
+    "footer",
+    null,
+    <small>&copy; {new Date().getFullYear()}. All rights reserved.</small>,
+  );
 }
 
 export default App;
